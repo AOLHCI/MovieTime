9b7976f9c2434bff0ecb061796174828c2b49ff9
FerlieHernata <ferlie.hernata01@gmail.com>
2024-05-11 02:40:19 +0700

    added 6 more trending

--- JavaScripts/community.js
+++ JavaScripts/community.js
@@ -651,81 +651,321 @@ function postCommentpopup() {
 
 var comments = [
   {
-    name: 'John',
-    time: '09:21 - 10 May 2024',
-    text: 'Great movie!',
-    likecount: 5,
-    replycount: 2,
-    reply: [
+      name: 'John',
+      time: '09:21 - 10 May 2024',
+      text: 'Great movie!',
+      likecount: 5,
+      replycount: 2,
+      reply: [
       {
-        name: 'Sarah',
-        time: '14:47 - 10 May 2024',
-        text: 'I agree, it was amazing!',
-        likecount: 3,
-        replycount: 0,
-        reply: [],
-        hashtag: 'none'
+          name: 'Sarah',
+          time: '14:47 - 10 May 2024',
+          text: 'I agree, it was amazing!',
+          likecount: 3,
+          replycount: 0,
+          reply: [],
+          hashtag: 'none'
       },
       {
-        name: 'Michael',
-        time: '03:15 - 11 May 2024',
-        text: 'Epic battle scenes!',
-        likecount: 2,
-        replycount: 0,
-        reply: [],
-        hashtag: 'none'
+          name: 'Michael',
+          time: '03:15 - 11 May 2024',
+          text: 'Epic battle scenes!',
+          likecount: 2,
+          replycount: 0,
+          reply: [],
+          hashtag: 'none'
       }
-    ],
-    hashtag: '#GodzillaXKong'
+      ],
+      hashtag: '#GodzillaXKong'
   },
   {
-    name: 'Emma',
-    time: '20:36 - 11 May 2024',
-    text: 'I loved the character development!',
-    likecount: 8,
-    replycount: 1,
-    reply: [
+      name: 'Emma',
+      time: '20:36 - 11 May 2024',
+      text: 'I loved the character development!',
+      likecount: 8,
+      replycount: 1,
+      reply: [
       {
-        name: 'Alex',
-        time: '08:59 - 12 May 2024',
-        text: 'Yes, it was so well done!',
-        likecount: 4,
-        replycount: 0,
-        reply: [],
-        hashtag: 'none'
+          name: 'Alex',
+          time: '08:59 - 12 May 2024',
+          text: 'Yes, it was so well done!',
+          likecount: 4,
+          replycount: 0,
+          reply: [],
+          hashtag: 'none'
       }
-    ],
-    hashtag: '#QueenOfTears'
+      ],
+      hashtag: '#QueenOfTears'
   },
   {
-    name: 'Lisa',
-    time: '16:22 - 12 May 2024',
-    text: 'Can\'t wait for the next season!',
-    likecount: 6,
-    replycount: 1,
-    reply: [
+      name: 'Lisa',
+      time: '16:22 - 12 May 2024',
+      text: 'Can\'t wait for the next season!',
+      likecount: 6,
+      replycount: 1,
+      reply: [
       {
-        name: 'David',
-        time: '11:04 - 13 May 2024',
-        text: 'Me too! It\'s going to be epic!',
-        likecount: 2,
-        replycount: 0,
-        reply: [],
-        hashtag: 'none'
+          name: 'David',
+          time: '11:04 - 13 May 2024',
+          text: 'Me too! It\'s going to be epic!',
+          likecount: 2,
+          replycount: 0,
+          reply: [],
+          hashtag: 'none'
       }
-    ],
-    hashtag: '#Exhuma'
+      ],
+      hashtag: '#Exhuma'
   },
   {
-    name: 'Chris',
-    time: '22:10 - 13 May 2024',
-    text: 'The storyline was gripping!',
-    likecount: 7,
-    replycount: 0,
-    reply: [],
-    hashtag: '#GameOfThrones'
+      name: 'Chris',
+      time: '22:10 - 13 May 2024',
+      text: 'The storyline was gripping!',
+      likecount: 7,
+      replycount: 0,
+      reply: [],
+      hashtag: '#GameOfThrones'
   },
-];
+  {
+      name: 'Sophia',
+      time: '03:56 - 18 May 2024',
+      text: 'Conjuring 4 will be a blast!',
+      likecount: 8,
+      replycount: 2,
+      reply: [
+          {
+              name: 'Daniel',
+              time: '12:09 - 18 May 2024',
+              text: 'Absolutely! The Conjuring series is top-notch!',
+              likecount: 3,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Conjuring4',
+          },
+          {
+              name: 'Aiden',
+              time: '08:33 - 19 May 2024',
+              text: 'I\'ve been counting down the days!',
+              likecount: 4,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Conjuring4',
+          }
+      ],
+      hashtag: '#Conjuring4'
+  },
+  {
+      name: 'Sarah',
+      time: '06:55 - 23 May 2024',
+      text: 'Can\'t wait for the next installment of Marvels!',
+      likecount: 12,
+      replycount: 3,
+      reply: [
+          {
+              name: 'Tom',
+              time: '15:10 - 23 May 2024',
+              text: 'Me too! Marvels movies are always a blast!',
+              likecount: 7,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Marvels'
+          },
+          {
+              name: 'Grace',
+              time: '09:25 - 24 May 2024',
+              text: 'I hope they introduce some new heroes!',
+              likecount: 4,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Marvels'
+          },
+          {
+              name: 'Sophia',
+              time: '11:48 - 25 May 2024',
+              text: 'I\'m excited for the plot twists!',
+              likecount: 6,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Marvels'
+          }
+      ],
+      hashtag: '#Marvels'
+  },
+  {
+      name: 'Rachel',
+      time: '05:30 - 31 May 2024',
+      text: 'Just watched Shang-Chi and it was fantastic!',
+      likecount: 18,
+      replycount: 3,
+      reply: [
+          {
+              name: 'Michael',
+              time: '14:45 - 31 May 2024',
+              text: 'One of the best Marvel movies in recent years!',
+              likecount: 9,
+              replycount: 1,
+              reply: [],
+              hashtag: '#ShangChi'
+          },
+          {
+              name: 'Emma',
+              time: '10:20 - 1 June 2024',
+              text: 'The soundtrack was incredible too!',
+              likecount: 7,
+              replycount: 0,
+              reply: [],
+              hashtag: '#ShangChi'
+          },
+          {
+              name: 'David',
+              time: '12:48 - 2 June 2024',
+              text: 'I\'m already planning to rewatch it!',
+              likecount: 10,
+              replycount: 1,
+              reply: [],
+              hashtag: '#ShangChi'
+          }
+      ],
+      hashtag: '#ShangChi'
+  },
+  {
+      name: 'Hye',
+      time: '06:40 - 7 June 2024',
+      text: 'Vincenzo is a masterpiece!',
+      likecount: 30,
+      replycount: 4,
+      reply: [
+          {
+              name: 'Jae',
+              time: '14:25 - 7 June 2024',
+              text: 'Absolutely! It\'s one of the best K-dramas!',
+              likecount: 16,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Vincenzo'
+          },
+          {
+              name: 'Hyun',
+              time: '11:55 - 8 June 2024',
+              text: 'I\'m recommending Vincenzo to all my friends!',
+              likecount: 22,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Vincenzo'
+          },
+          {
+              name: 'Mi',
+              time: '09:35 - 9 June 2024',
+              text: 'Vincenzo deserves all the praise!',
+              likecount: 25,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Vincenzo'
+          },
+          {
+              name: 'Sung',
+              time: '18:20 - 9 June 2024',
+              text: 'I\'m eagerly waiting for the next episode!',
+              likecount: 28,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Vincenzo'
+          }
+      ],
+      hashtag: '#Vincenzo'
+  },
+  {
+      name: 'Daniel',
+      time: '08:45 - 15 June 2024',
+      text: 'Madame Web\'s lore is so rich!',
+      likecount: 40,
+      replycount: 5,
+      reply: [
+          {
+              name: 'Emma',
+              time: '14:10 - 15 June 2024',
+              text: 'Absolutely! She\'s a pivotal figure in the Spider-Verse!',
+              likecount: 22,
+              replycount: 0,
+              reply: [],
+              hashtag: '#MadameWeb'
+          },
+          {
+              name: 'Ella',
+              time: '12:55 - 16 June 2024',
+              text: 'Madame Web\'s influence reaches far beyond just Spider-Man!',
+              likecount: 28,
+              replycount: 0,
+              reply: [],
+              hashtag: '#MadameWeb'
+          },
+          {
+              name: 'Chloe',
+              time: '09:35 - 17 June 2024',
+              text: 'I\'m eagerly waiting for more Madame Web appearances!',
+              likecount: 35,
+              replycount: 0,
+              reply: [],
+              hashtag: '#MadameWeb'
+          },
+          {
+              name: 'Tyler',
+              time: '18:20 - 17 June 2024',
+              text: 'Madame Web adds a whole new layer to the Spider-Man mythos!',
+              likecount: 38,
+              replycount: 0,
+              reply: [],
+              hashtag: '#MadameWeb'
+          }
+      ],
+      hashtag: '#MadameWeb'
+  },
+  {
+      name: 'Lily',
+      time: '07:45 - 24 June 2024',
+      text: 'The Wonka movie is going to be a cinematic treat!',
+      likecount: 55,
+      replycount: 6,
+      reply: [
+          {
+              name: 'Mason',
+              time: '14:10 - 24 June 2024',
+              text: 'Absolutely! I\'m excited to revisit the chocolate factory!',
+              likecount: 38,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Wonka'
+          },
+          {
+              name: 'Oscar',
+              time: '12:25 - 25 June 2024',
+              text: 'I hope the new Wonka movie stays true to the spirit of the original!',
+              likecount: 42,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Wonka'
+          },
+          {
+              name: 'Quinn',
+              time: '09:35 - 26 June 2024',
+              text: 'I hope the new Wonka movie sparks the imagination of a new generation!',
+              likecount: 48,
+              replycount: 0,
+              reply: [],
+              hashtag: '#Wonka'
+          },
+      ],
+      hashtag: '#Wonka'
+  },
+  {
+      name: 'Rachel',
+      time: '18:20 - 26 June 2024',
+      text: 'The Wonka movie has so much potential for creativity!',
+      likecount: 52,
+      replycount: 3,
+      reply: [],
+      hashtag: '#Wonka'
+  }
+  ];
 
 document.querySelectorAll('.Trend-info').forEach(element => {
   element.addEventListener('click', function(event) {
